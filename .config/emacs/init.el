08f62d172095335854b5f8eba65690026918708c
Ivan Chien <qyc027@outlook.com>
2026-03-01 15:30:37 +0800

    fix(emacs): expand-file-name elisp scripts on config root

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,4 +1,11 @@
-(setq custom-file (expand-file-name "custom.el"))
+(defconst my-root user-emacs-directory)
+
+;; (defconst my-lisp-dir
+;;   (expand-file-name "lisp/" my-root))
+
+;; (add-to-list 'load-path my-lisp-dir)
+
+(setq custom-file (expand-file-name "custom.el" my-root))
 
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -7,11 +14,15 @@
 
 (show-paren-mode t)
 
-(load (expand-file-name "themes/nordic-night/nordic-night-theme.el"))
+(load (expand-file-name "themes/nordic-night/nordic-night-theme.el" my-root))
 ;; (load (expand-file-name "themes/nordic-night/nordic-midnight-theme.el"))
 (load-theme 'nordic-night :no-confirm)
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
+
+(fido-mode t)
+
+(winner-mode 1)
 
 (load custom-file)
